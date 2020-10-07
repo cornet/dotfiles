0f8eefabc179071c53ede001ea7c0dcab5b226d3
cornet <cornet@sheepy.org>
2020-10-07 12:45:13 +0100

    Switch to using yabai

---
+++ hammerspoon/.hammerspoon/hyper.lua
@@ -0,0 +1,97 @@
+local This = {}
+
+-----------------------------------------------------------------------------------
+-- File: hyper.lua
+-- Author: J.H. Kuperus
+-- Source: https://github.com/jhkuperus/dotfiles/blob/master/hammerspoon/hyper.lua
+-- "License": Feel free to use this file any way you like. Issues or improvements
+--            are welcome on the GitHub repository. No warranties whatsoever.
+-----------------------------------------------------------------------------------
+
+-- To use Hyper in your init.lua script, import it and adapt this example to
+-- your needs:
+-- 
+-- local hyper = require('hyper')
+-- hyper.install('F18') 
+-- hyper.bindkey('r', hs.reload)
+--
+-- The above three lines initialize Hyper to respond to F18 key-events and binds
+-- Hyper+r to Hammerspoon Reload (easy way to refresh Hammerspoon's config)
+
+-- Hyper mode needs to be bound to a key. Here we are binding
+-- it to F17, because this is yet another key that's unused.
+-- Why not F18? We will use key-events from F18 to turn hyper
+-- mode on and off. Using F17 as the modal and source of key
+-- events, will result in a very jittery Hyper mode.
+
+This.hyperMode = hs.hotkey.modal.new({}, 'F17')
+
+local hyper_enabled = false
+
+-- Enter Hyper Mode when F18 (Hyper) is pressed
+function enterHyperMode()
+    This.hyperMode:enter()
+    hs.alert.show("Entered Hyper", 1)
+    hyper_enabled = true
+end
+
+-- Leave Hyper Mode when F18 (Hyper) is pressed
+function exitHyperMode()
+   This.hyperMode:exit()
+   hs.alert.show("Exited Hyper", 1)
+   hyper_enabled = false
+end
+
+function toggleHyperMode()
+  if not hyper_enabled then
+    This.hyperMode:enter()
+    hs.alert.show("Entered Hyper", 1)
+    hyper_enabled = true
+  else 
+    This.hyperMode:exit()
+    hs.alert.show("Exited Hyper", 1)
+    hyper_enabled = false
+  end
+end
+
+-- Utility to bind handler to Hyper+key
+function This.bindKey(key, handler)
+    This.hyperMode:bind({}, key, handler)
+end
+
+-- Utility to bind handler to Hyper+Shift+key
+function This.bindShiftKey(key, handler)
+   This.hyperMode:bind({'shift'}, key, handler)
+end
+
+-- Utility to bind handler to Hyper+Command+Shift+key
+function This.bindCommandShiftKey(key, handler)
+  This.hyperMode:bind({'command', 'shift'}, key, handler)
+end
+
+-- Utility to bind handler to Hyper+modifiers+key
+function This.bindKeyWithModifiers(mods, key, handler)
+   This.hyperMode:bind(mods, key, handler)
+end
+
+-- Binds the enter/exit functions of the Hyper modal to all combinations of modifiers
+function This.install(hotKey)
+  hs.hotkey.bind({}, hotKey, toggleHyperMode)
+--  hs.hotkey.bind({"shift"}, hotKey, enterHyperMode, exitHyperMode)
+--  hs.hotkey.bind({"ctrl"}, hotKey, enterHyperMode, exitHyperMode)
+--  hs.hotkey.bind({"ctrl", "shift"}, hotKey, enterHyperMode, exitHyperMode)
+--  hs.hotkey.bind({"cmd"}, hotKey, enterHyperMode, exitHyperMode)
+--  hs.hotkey.bind({"cmd", "shift"}, hotKey, enterHyperMode, exitHyperMode)
+--  hs.hotkey.bind({"cmd", "ctrl"}, hotKey, enterHyperMode, exitHyperMode)
+--  hs.hotkey.bind({"cmd", "ctrl", "shift"}, hotKey, enterHyperMode, exitHyperMode)
+--  hs.hotkey.bind({"alt"}, hotKey, enterHyperMode, exitHyperMode)
+--  hs.hotkey.bind({"alt", "shift"}, hotKey, enterHyperMode, exitHyperMode)
+--  hs.hotkey.bind({"alt", "ctrl"}, hotKey, enterHyperMode, exitHyperMode)
+--  hs.hotkey.bind({"alt", "ctrl", "shift"}, hotKey, enterHyperMode, exitHyperMode)
+--  hs.hotkey.bind({"alt", "cmd"}, hotKey, enterHyperMode, exitHyperMode)
+--  hs.hotkey.bind({"alt", "cmd", "shift"}, hotKey, enterHyperMode, exitHyperMode)
+--  hs.hotkey.bind({"alt", "cmd", "ctrl"}, hotKey, enterHyperMode, exitHyperMode)
+--  hs.hotkey.bind({"alt", "cmd", "shift", "ctrl"}, hotKey, enterHyperMode, exitHyperMode)
+end
+
+return This
--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -11,154 +11,226 @@
 -- end
 -- end)
 
-hs.hotkey.bind({"alt", "shift"}, "return", function()
+local hyper = require('hyper')
+hyper.install('F18') 
+hyper.bindKeyWithModifiers({"alt", "shift"}, 'r', hs.reload)
+
+-- hs.hotkey.bind({"alt", "shift"}, "return", function()
+-- 	hs.execute("open -na kitty")
+-- end)
+
+hyper.bindKeyWithModifiers({"alt", "shift"}, "return", function()
 	hs.execute("open -na kitty")
 end)
 
 -- Disable Window Animations or it's slooww....
 hs.window.animationDuration = 0
 
--- Window Placement Hotkeys
--- Fullscreen
-hs.hotkey.bind({"cmd", "shift"}, "return", function()
-	if hs.window.focusedWindow() then
-		local win = hs.window.focusedWindow()
-		local f = win:frame()
-		local screen = win:screen()
-		local geo = screen:frame()
-
-		f.x = geo.x
-		f.y = geo.y 
-		f.w = geo.w
-		f.h = geo.h
-		win:setFrame(f)
-	end
+hyper.bindKeyWithModifiers({"cmd", "shift"}, "return", function()
+  hs.execute('/usr/local/bin/yabai -m window --toggle zoom-fullscreen')
 end)
 
--- Top 1/2
-hs.hotkey.bind({"cmd", "shift"}, "k", function()
-	if hs.window.focusedWindow() then
-		local win = hs.window.focusedWindow()
-		local f = win:frame()
-		local screen = win:screen()
-		local geo = screen:frame()
 
-		f.x = geo.x + 10
-		f.y = geo.y + 10
-		f.w = geo.w - 20
-		f.h = geo.h / 2 - 20
-		win:setFrame(f)
-	end
+hyper.bindKeyWithModifiers({"cmd", "shift"}, "space", function()
+  hs.execute('/usr/local/bin/yabai -m space --rotate 90')
 end)
 
--- Bottom 1/2
-hs.hotkey.bind({"cmd", "shift"}, "j", function()
-	if hs.window.focusedWindow() then
-		local win = hs.window.focusedWindow()
-		local f = win:frame()
-		local screen = win:screen()
-		local geo = screen:frame()
 
-		f.x = geo.x + 10
-		f.y = geo.y + ( geo.h / 2) + 10
-		f.w = geo.w - 20
-		f.h = geo.h / 2 - 20
-		win:setFrame(f)
-	end
+-- Focus Windows
+hyper.bindKeyWithModifiers({"alt"}, "k", function()
+  hs.execute('/usr/local/bin/yabai -m window --focus north')
 end)
 
--- Left 1/2
-hs.hotkey.bind({"cmd", "shift"}, "h", function()
-	if hs.window.focusedWindow() then
-		local win = hs.window.focusedWindow()
-		local f = win:frame()
-		local screen = win:screen()
-		local geo = screen:frame()
-
-		f.x = geo.x + 10
-		f.y = geo.y + 10
-		f.w = geo.w / 2 - 20
-		f.h = geo.h - 20
-		win:setFrame(f)
-	end
+hyper.bindKeyWithModifiers({"alt"}, "j", function()
+  hs.execute('/usr/local/bin/yabai -m window --focus south')
 end)
 
--- Right 1/2
-hs.hotkey.bind({"cmd", "shift"}, "l", function()
-	if hs.window.focusedWindow() then
-		local win = hs.window.focusedWindow()
-		local f = win:frame()
-		local screen = win:screen()
-		local geo = screen:frame()
-
-		f.x = geo.x + (geo.w / 2) + 10
-		f.y = geo.y + 10
-		f.w = geo.w / 2 - 20
-		f.h = geo.h - 20
-		win:setFrame(f)
-	end
+hyper.bindKeyWithModifiers({"alt"}, "l", function()
+  hs.execute('/usr/local/bin/yabai -m window --focus east')
 end)
 
--- Top left
-hs.hotkey.bind({"cmd", "shift"}, "y", function()
-	if hs.window.focusedWindow() then
-		local win = hs.window.focusedWindow()
-		local f = win:frame()
-		local screen = win:screen()
-		local geo = screen:frame()
-
-		f.x = geo.x + 10
-		f.y = geo.y + 10
-		f.w = geo.w / 2 - 20 
-		f.h = geo.h / 2 - 20
-		win:setFrame(f)
-	end
+hyper.bindKeyWithModifiers({"alt"}, "h", function()
+  hs.execute('/usr/local/bin/yabai -m window --focus west')
 end)
 
--- Top right
-hs.hotkey.bind({"cmd", "shift"}, "p", function()
-	if hs.window.focusedWindow() then
-		local win = hs.window.focusedWindow()
-		local f = win:frame()
-		local screen = win:screen()
-		local geo = screen:frame()
 
-		f.x = geo.x + (geo.w / 2) + 10
-		f.y = geo.y + 10
-		f.w = geo.w / 2 - 20
-		f.h = geo.h / 2 - 20
-		win:setFrame(f)
-	end
+-- Swap Windows
+hyper.bindKeyWithModifiers({"alt", "shift"}, "k", function()
+  hs.execute('/usr/local/bin/yabai -m window --swap north')
 end)
 
--- Bottom right
-hs.hotkey.bind({"cmd", "shift"}, ".", function()
-	if hs.window.focusedWindow() then
-		local win = hs.window.focusedWindow()
-		local f = win:frame()
-		local screen = win:screen()
-		local geo = screen:frame()
-
-		f.x = geo.x + (geo.w / 2) + 10
-		f.y = geo.y + (geo.h / 2) + 10
-		f.w = geo.w / 2 - 20
-		f.h = geo.h / 2 - 20
-		win:setFrame(f)
-	end
+hyper.bindKeyWithModifiers({"alt", "shift"}, "j", function()
+  hs.execute('/usr/local/bin/yabai -m window --swap south')
 end)
 
--- Bottom left
-hs.hotkey.bind({"cmd", "shift"}, "b", function()
-	if hs.window.focusedWindow() then
-		local win = hs.window.focusedWindow()
-		local f = win:frame()
-		local screen = win:screen()
-		local geo = screen:frame()
-
-		f.x = geo.x + 10
-		f.y = geo.y + (geo.h / 2) + 10
-		f.w = geo.w / 2 - 20
-		f.h = geo.h / 2 - 20
-		win:setFrame(f)
-	end
+hyper.bindKeyWithModifiers({"alt", "shift"}, "l", function()
+  hs.execute('/usr/local/bin/yabai -m window --swap east')
 end)
+
+hyper.bindKeyWithModifiers({"alt", "shift"}, "h", function()
+  hs.execute('/usr/local/bin/yabai -m window --swap west')
+end)
+
+-- Warp Windows
+hyper.bindKeyWithModifiers({"cmd", "shift"}, "k", function()
+  hs.execute('/usr/local/bin/yabai -m window --warp north')
+end)
+
+hyper.bindKeyWithModifiers({"cmd", "shift"}, "j", function()
+  hs.execute('/usr/local/bin/yabai -m window --warp south')
+end)
+
+hyper.bindKeyWithModifiers({"cmd", "shift"}, "l", function()
+  hs.execute('/usr/local/bin/yabai -m window --warp east')
+end)
+
+hyper.bindKeyWithModifiers({"cmd", "shift"}, "h", function()
+  hs.execute('/usr/local/bin/yabai -m window --warp west')
+end)
+
+
+
+-- -- Window Placement Hotkeys
+-- -- Fullscreen
+-- hs.hotkey.bind({"cmd", "shift"}, "return", function()
+-- 	if hs.window.focusedWindow() then
+-- 		local win = hs.window.focusedWindow()
+-- 		local f = win:frame()
+-- 		local screen = win:screen()
+-- 		local geo = screen:frame()
+-- 
+-- 		f.x = geo.x
+-- 		f.y = geo.y 
+-- 		f.w = geo.w
+-- 		f.h = geo.h
+-- 		win:setFrame(f)
+-- 	end
+-- end)
+-- 
+-- -- Top 1/2
+-- hs.hotkey.bind({"cmd", "shift"}, "k", function()
+-- 	if hs.window.focusedWindow() then
+-- 		local win = hs.window.focusedWindow()
+-- 		local f = win:frame()
+-- 		local screen = win:screen()
+-- 		local geo = screen:frame()
+-- 
+-- 		f.x = geo.x + 10
+-- 		f.y = geo.y + 10
+-- 		f.w = geo.w - 20
+-- 		f.h = geo.h / 2 - 20
+-- 		win:setFrame(f)
+-- 	end
+-- end)
+-- 
+-- -- Bottom 1/2
+-- hs.hotkey.bind({"cmd", "shift"}, "j", function()
+-- 	if hs.window.focusedWindow() then
+-- 		local win = hs.window.focusedWindow()
+-- 		local f = win:frame()
+-- 		local screen = win:screen()
+-- 		local geo = screen:frame()
+-- 
+-- 		f.x = geo.x + 10
+-- 		f.y = geo.y + ( geo.h / 2) + 10
+-- 		f.w = geo.w - 20
+-- 		f.h = geo.h / 2 - 20
+-- 		win:setFrame(f)
+-- 	end
+-- end)
+-- 
+-- -- Left 1/2
+-- hs.hotkey.bind({"cmd", "shift"}, "h", function()
+-- 	if hs.window.focusedWindow() then
+-- 		local win = hs.window.focusedWindow()
+-- 		local f = win:frame()
+-- 		local screen = win:screen()
+-- 		local geo = screen:frame()
+-- 
+-- 		f.x = geo.x + 10
+-- 		f.y = geo.y + 10
+-- 		f.w = geo.w / 2 - 20
+-- 		f.h = geo.h - 20
+-- 		win:setFrame(f)
+-- 	end
+-- end)
+-- 
+-- -- Right 1/2
+-- hs.hotkey.bind({"cmd", "shift"}, "l", function()
+-- 	if hs.window.focusedWindow() then
+-- 		local win = hs.window.focusedWindow()
+-- 		local f = win:frame()
+-- 		local screen = win:screen()
+-- 		local geo = screen:frame()
+-- 
+-- 		f.x = geo.x + (geo.w / 2) + 10
+-- 		f.y = geo.y + 10
+-- 		f.w = geo.w / 2 - 20
+-- 		f.h = geo.h - 20
+-- 		win:setFrame(f)
+-- 	end
+-- end)
+-- 
+-- -- Top left
+-- hs.hotkey.bind({"cmd", "shift"}, "y", function()
+-- 	if hs.window.focusedWindow() then
+-- 		local win = hs.window.focusedWindow()
+-- 		local f = win:frame()
+-- 		local screen = win:screen()
+-- 		local geo = screen:frame()
+-- 
+-- 		f.x = geo.x + 10
+-- 		f.y = geo.y + 10
+-- 		f.w = geo.w / 2 - 20 
+-- 		f.h = geo.h / 2 - 20
+-- 		win:setFrame(f)
+-- 	end
+-- end)
+-- 
+-- -- Top right
+-- hs.hotkey.bind({"cmd", "shift"}, "p", function()
+-- 	if hs.window.focusedWindow() then
+-- 		local win = hs.window.focusedWindow()
+-- 		local f = win:frame()
+-- 		local screen = win:screen()
+-- 		local geo = screen:frame()
+-- 
+-- 		f.x = geo.x + (geo.w / 2) + 10
+-- 		f.y = geo.y + 10
+-- 		f.w = geo.w / 2 - 20
+-- 		f.h = geo.h / 2 - 20
+-- 		win:setFrame(f)
+-- 	end
+-- end)
+-- 
+-- -- Bottom right
+-- hs.hotkey.bind({"cmd", "shift"}, ".", function()
+-- 	if hs.window.focusedWindow() then
+-- 		local win = hs.window.focusedWindow()
+-- 		local f = win:frame()
+-- 		local screen = win:screen()
+-- 		local geo = screen:frame()
+-- 
+-- 		f.x = geo.x + (geo.w / 2) + 10
+-- 		f.y = geo.y + (geo.h / 2) + 10
+-- 		f.w = geo.w / 2 - 20
+-- 		f.h = geo.h / 2 - 20
+-- 		win:setFrame(f)
+-- 	end
+-- end)
+-- 
+-- -- Bottom left
+-- hs.hotkey.bind({"cmd", "shift"}, "b", function()
+-- 	if hs.window.focusedWindow() then
+-- 		local win = hs.window.focusedWindow()
+-- 		local f = win:frame()
+-- 		local screen = win:screen()
+-- 		local geo = screen:frame()
+-- 
+-- 		f.x = geo.x + 10
+-- 		f.y = geo.y + (geo.h / 2) + 10
+-- 		f.w = geo.w / 2 - 20
+-- 		f.h = geo.h / 2 - 20
+-- 		win:setFrame(f)
+-- 	end
+-- end)
