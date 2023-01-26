9f9af5e483300b0219d0d3d52b6d6d14226985f5
cornet <cornet@sheepy.org>
2023-01-26 18:05:45 +0000

    Fix keymapps + shellcheck

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -4,22 +4,10 @@ if not present then
   return
 end
 
-lint.linters.tflint = {
-  cmd = 'tflint',
-  stdin = false,
-  args = {'--format', 'compact', '--no-color'},
-  stream = 'stdout',
-  ignore_exitcode = false,
-  env = nil,
-  -- main.tf:3:19: Error - "t1.2xlarge" is an invalid value as instance_type (aws_instance_invalid_type)
-  parser = require('lint.parser').from_errorformat('%f:%l:%c: %t%*[a-z] - %m', {
-    source = 'tflint',
-  })
-}
-
+-- Most linters are installed and configured by Mason.nvim
+-- however some don't work for various reasons.
 lint.linters_by_ft = {
-  terraform = {'tflint'},
-  sh = {'shellcheck'}
+  sh = {'shellcheck'} -- Mason one doesn't work on M1 Macs
 }
 
 vim.cmd([[
