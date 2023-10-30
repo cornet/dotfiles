55c5a8ee1e2feefe66a9b7a22eafd01383c285e1
cornet <cornet@sheepy.org>
2023-10-30 18:44:58 +0000

    neo-tree: Enable window picker & auto follow

--- nvim/.config/nvim/lua/plugins/interface.lua
+++ nvim/.config/nvim/lua/plugins/interface.lua
@@ -42,6 +42,25 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
+      {
+        's1n7ax/nvim-window-picker',
+        version = '2.*',
+        config = function()
+          require 'window-picker'.setup({
+            filter_rules = {
+              include_current_win = false,
+              autoselect_one = true,
+              -- filter using buffer options
+              bo = {
+                -- if the file type is one of following, the window will be ignored
+                filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+                -- if the buffer type is one of following, the window will be ignored
+                buftype = { 'terminal', "quickfix" },
+              },
+            },
+          })
+        end,
+      },
     },
     keys = {
       {'<leader>ne', '<cmd>Neotree toggle<cr>', {desc = 'nvim-tree', noremap = true, silent = true}},
@@ -57,6 +76,10 @@ return {
               "--exclude",
               "node_modules",
             },
+          },
+          follow_current_file = {
+            enabled = true,
+            leave_dirs_open = true
           }
         },
         window = {
