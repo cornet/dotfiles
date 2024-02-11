5bc42e52a6956138c506a632f49b88b7355894e3
cornet <cornet@sheepy.org>
2024-02-11 18:57:29 +0000

    Added tmux navigator. Fixed cmp key bindings

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.ruby" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     -- import/override with your plugins
     { import = "plugins" },
