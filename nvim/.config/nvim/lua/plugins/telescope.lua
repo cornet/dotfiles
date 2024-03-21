df6db1fe519ce2b00397bbc5770cb9871c4eeff0
cornet <cornet@sheepy.org>
2024-03-21 12:36:06 +0000

    Add key binding for terraform docs telescope plugin

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,20 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  keys = {
-    -- Free up ,gs for fugative key binding
-    { "<leader>gs", false },
-    { "<leader>gS", "<cmd>Telescope git_status<CR>", desc = "status" },
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- Free up ,gs for fugative key binding
+      { "<leader>gs", false },
+      { "<leader>gS", "<cmd>Telescope git_status<CR>", desc = "status" },
+    },
+  },
+  {
+    "ANGkeith/telescope-terraform-doc.nvim",
+    keys = {
+      {
+        "<leader>hta",
+        "<cmd>Telescope terraform_doc full_name=hashicorp/aws<cr>",
+        desc = "aws provider documentation",
+      },
+    },
   },
 }
