fac8a025f1c96dabe4a2e4ad02f4be1daa865143
cornet <cornet@sheepy.org>
2026-03-24 22:59:19 +0000

    Sync from work laptop

--- nvim/.config/nvim/lua/plugins/interface.lua
+++ nvim/.config/nvim/lua/plugins/interface.lua
@@ -1,27 +1,30 @@
 return {
   {
-    -- Move consistently between windows in Tmux & Vim
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-    },
+    "swaits/zellij-nav.nvim",
+    lazy = true,
+    event = "VeryLazy",
     keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+      { "<c-h>", "<cmd>ZellijNavigateLeftTab<cr>", { silent = true, desc = "navigate left or tab" } },
+      { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+      { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
+      { "<c-l>", "<cmd>ZellijNavigateRightTab<cr>", { silent = true, desc = "navigate right or tab" } },
     },
+    opts = {},
   },
 
   {
     "folke/snacks.nvim",
     opts = {
+      gh = {},
       picker = {
+        gh_issue = {
+          -- your gh_issue picker configuration comes here
+          -- or leave it empty to use the default settings
+        },
+        gh_pr = {
+          -- your gh_pr picker configuration comes here
+          -- or leave it empty to use the default settings
+        },
         formatters = {
           file = {
             truncate = 80,
